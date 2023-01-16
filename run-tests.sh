41763e7ebd80cb2f975e9bbfcc564c456018b36b
anikachurilova <nyuta.churilova@gmail.com>
2023-01-16 14:48:23 +0100

    global: move from setup.py to setup.cfg

--- run-tests.sh
+++ run-tests.sh
@@ -13,13 +13,13 @@ set -o errexit
 set -o nounset
 # Always bring down docker services
 function cleanup() {
-    eval "$(docker-services-cli down --env)"
-}
+   eval "$(docker-services-cli down --env)"
+ }
 trap cleanup EXIT
 
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --env)"
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-redis} --env)"
 python -m pytest
 tests_exit_code=$?
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
