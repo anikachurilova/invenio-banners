fc603530ae93a880953e56485ad95e94a9a6d7ed
anikachurilova <nyuta.churilova@gmail.com>
2023-02-02 14:23:27 +0100

    administration: integrate banners view module
    * integrate admin banners list, edit, create and details view
    * disable expired banners after each create and update action
    * improve banners marshmallow schema
    * add http request data validation at create and update actions
    * improve banners macro
    * closes https://github.com/inveniosoftware/invenio-banners/issues/10

--- invenio_banners/ext.py
+++ invenio_banners/ext.py
@@ -7,6 +7,8 @@
 
 """Invenio extension app."""
 
+from flask import Blueprint
+
 from . import config
 from .resources import BannerResource, BannerResourceConfig
 from .services import BannerService, BannerServiceConfig
