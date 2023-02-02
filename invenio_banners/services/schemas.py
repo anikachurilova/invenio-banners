aee5279c85419f9b60b9d031b3180852f6801081
anikachurilova <nyuta.churilova@gmail.com>
2023-02-02 14:23:27 +0100

    administration: integrate banners view module
    * integrate admin banners list, edit, create and details view
    * disable expired banners after each create and update action
    * improve banners marshmallow schema
    * add http request data validation at create and update actions
    * improve banners macro
    * closes https://github.com/inveniosoftware/invenio-banners/issues/10

--- invenio_banners/services/schemas.py
+++ invenio_banners/services/schemas.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 CERN.
+# Copyright (C) 2022-2023 CERN.
 #
 # Invenio-Banners is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -14,9 +14,11 @@ from marshmallow import fields
 class BannerSchema(BaseRecordSchema):
     """Schema for banners."""
 
-    message = fields.String()
-    url_path = fields.String()
-    category = fields.String()
-    start_datetime = fields.DateTime()
-    end_datetime = fields.DateTime()
-    active = fields.Boolean()
+    message = fields.String(required=True)
+    url_path = fields.String(allow_none=True)
+    category = fields.String(required=True)
+    start_datetime = fields.DateTime(required=True)
+    end_datetime = fields.DateTime(allow_none=True)
+    active = fields.Boolean(required=True)
+    created = fields.DateTime(dump_only=True)
+    updated = fields.DateTime(dump_only=True)
